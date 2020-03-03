30b50642763dde9548f305f777138aea0bdeef8f
xiegangd <xiegangd@gmail.com>
2020-03-03 17:20:10 +0800

    add Weather

---
+++ src/Weather.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Xiegangd\Weather;
+
+use GuzzleHttp\Client;
+
+class Weather
+{
+    protected $key;
+    protected $guzzleOptions;
+
+    /**
+     * Weather constructor.
+     * @param $key
+     */
+    public function __construct($key)
+    {
+        $this->key = $key;
+    }
+
+    public function getHttpClient()
+    {
+        return new Client($this->guzzleOptions);
+    }
+
+    public function setGuzzleOptions(array $options)
+    {
+        $this->guzzleOptions = $options;
+    }
+
+    public function getWeather($city, $type = 'base', $format = 'json')
+    {
+        $url = 'https://restapi.amap.com/v3/weather/weatherInfo';
+
+        $query = array_filter([
+            'key' => $this->key,
+            'city' => $city,
+            'output' => $format,
+            'extensions' =>  $type,
+        ]);
+
+        $response = $this->getHttpClient()->get($url, [
+            'query' => $query,
+        ])->getBody()->getContents();
+
+        return 'json' === $format ? \json_decode($response, true) : $response;
+    }
+
+}
